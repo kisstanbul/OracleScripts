d96e8e7fe784fe8829377f4ae8ff4ed8338efdd7
kcelik <kcelik.istanbul@gmail.com>
2018-11-05 12:16:03 +0300

    Create ReCompile.sql
    Recompiles invalid objects.
    
    You may use:
    EXEC DBMS_UTILITY.compile_schema(schema => 'SCOTT', compile_all => false);

---
+++ ReCompile.sql
@@ -0,0 +1,17 @@
+BEGIN
+  FOR item IN (SELECT object_name, object_type, DECODE (object_type,  'PACKAGE', 1,  'PACKAGE BODY', 2,  3) AS recompile_order
+               FROM user_objects
+               WHERE status != 'VALID'
+               ORDER BY 3) LOOP
+    BEGIN
+      IF item.object_type <> 'PACKAGE BODY' THEN
+        EXECUTE IMMEDIATE 'ALTER ' || item.object_type || ' ' || item.object_name || ' COMPILE';
+      ELSE
+        EXECUTE IMMEDIATE 'ALTER PACKAGE ' || item.object_name || ' COMPILE BODY';
+      END IF;
+    EXCEPTION
+      WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line (item.object_type || ' : ' || item.object_name || ' : ' || DBMS_UTILITY.FORMAT_ERROR_STACK || '  ' || DBMS_UTILITY.format_error_backtrace);
+    END;
+  END LOOP;
+END;
